d32245ac0c08ee1be6da8c95849a326775917029
Ksenia <ksu606@ya.ru>
2022-11-26 17:07:52 +0300

    5

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createRoot } from 'react-dom/client';
+import {createTheme, CssBaseline, ThemeProvider} from '@material-ui/core';
+import {amber, green} from '@material-ui/core/colors';
 
-const container  = document.getElementById('root') as HTMLElement
+
+const theme= createTheme({
+    palette:{
+        primary: green,
+        secondary: amber,
+        type:'dark'
+
+    }
+})
+const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App />
+    </ThemeProvider>
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
